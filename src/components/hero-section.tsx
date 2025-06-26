6312624f8932b07d7c8a3c42afcebd5413dfa9cd
Shahbaz Khan <shahbazkhan19111@gmail.com>
2025-06-26 13:51:06 +0000

    image take full edge to edge height and width

--- src/components/hero-section.tsx
+++ src/components/hero-section.tsx
@@ -37,7 +37,7 @@ export function HeroSection() {
     }, []);
 
   return (
-    <section className="relative h-screen -mt-8 -mx-4 sm:-mx-6 lg:-mx-8 flex items-center justify-center text-center text-white overflow-hidden">
+    <section className="relative h-screen w-screen -mt-8 -ml-[50vw] left-1/2 flex items-center justify-center text-center text-white overflow-hidden">
         {carouselImages.map((image, index) => (
             <div
                 key={index}
